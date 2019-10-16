445d5242e160da83b073a628a8be3af8b07ba7e5
anderson <anderdavid86@hotmail.com>
2019-10-16 16:47:28 -0500

    se pone opcion logout en menu

--- app/src/main/java/com/smartappsolutions/MainActivity.kt
+++ app/src/main/java/com/smartappsolutions/MainActivity.kt
@@ -25,7 +25,13 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_settings -> {
+
+                Toast.makeText(applicationContext,"logOut",Toast.LENGTH_SHORT).show()
+                return true
+            }
+
+
             else -> super.onOptionsItemSelected(item)
         }
     }
